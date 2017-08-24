a4f341c22d6ff584e5e0ca358929e6badfcde47d
Matteo Giachino <matteo.giachino@prima.it>
2017-08-24 10:10:14 +0200

    adds metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Fetcher.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description,
+      package: package
     ]
   end
 
@@ -18,11 +20,19 @@ defmodule Fetcher.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
+  end
+
+  defp description do
+    "Fetcher allows to fetch list of data from data structures and getting back meaningful errors"
+  end
+
+  defp package do
+    %{maintainers: ["Matteo Giachino"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/matteosister/fetcher"}}
   end
 end
